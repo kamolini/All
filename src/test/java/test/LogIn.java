5f297acf1e85884e229cde2ff407a18bedc8ce8b
kamolini <kamolinibachar@gmail.com>
2016-03-28 16:08:27 -0400

    First Commit

---
+++ src/test/java/test/LogIn.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class LogIn {
+	
+	@Test
+	public void Userlogin(){
+		
+		System.out.println("LogIn");
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Main");
+	}
+
+}
---
+++ src/test/java/test/SmokeTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+	
+	@Test
+	public void t(){
+		
+		System.out.println("Kamolini");
+	}
+
+}
